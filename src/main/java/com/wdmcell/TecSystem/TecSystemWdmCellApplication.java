a873a530b07258b358f5f3e69d3138b64f29fbb0
MSantos4 <mraiza4@gmail.com>
2025-12-09 16:50:11 -0300

    projeto base

---
+++ src/main/java/com/wdmcell/TecSystem/TecSystemWdmCellApplication.java
@@ -0,0 +1,13 @@
+package com.wdmcell.TecSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecSystemWdmCellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecSystemWdmCellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdmcell/TecSystem/TecSystemWdmCellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdmcell.TecSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecSystemWdmCellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
