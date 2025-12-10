284e301c544a164834da9c7302089e403d91a54c
MSantos4 <mraiza4@gmail.com>
2025-12-10 15:13:25 -0300

    adicionar crud do produto

--- src/main/java/com/wdmcell/TecSystem/Controller/ProdutoController.java
+++ src/main/java/com/wdmcell/TecSystem/Controller/ProdutoController.java
@@ -1,9 +1,19 @@
 package com.wdmcell.TecSystem.Controller;
 
+import com.wdmcell.TecSystem.DTO.ClienteDTO;
+import com.wdmcell.TecSystem.DTO.ProdutoDTO;
+import com.wdmcell.TecSystem.DTO.Response.ClienteResponse;
+import com.wdmcell.TecSystem.DTO.Response.ProdutoResponse;
+import com.wdmcell.TecSystem.DTO.Response.Response;
 import com.wdmcell.TecSystem.Service.ProdutoService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,6 +22,100 @@ public class ProdutoController {
 
     private final ProdutoService produtoService;
 
+    @PostMapping
+    public ResponseEntity<Response<ProdutoResponse>> salvar(@RequestBody ProdutoDTO produtoDTO) {
+        try {
+            ProdutoResponse clienteResponse = produtoService.salvar(produtoDTO);
 
+            Response<ProdutoResponse> response = new Response<>(
+                    "Sucesso",
+                    "Produto cadastrado com sucesso!",
+                    LocalDateTime.now(),
+                    clienteResponse
+            );
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            Response<ProdutoResponse> response = new Response<>(
+                    "Erro",
+                    "Erro ao cadastrar produto: " + e.getMessage(),
+                    LocalDateTime.now()
+            );
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<List<ProdutoResponse>>> buscarProdutos() {
+        try {
+            List<ProdutoResponse> clientesResponse = produtoService.buscarClientes();
+
+            Response<List<ProdutoResponse>> response = new Response<>(
+                    "Sucesso",
+                    "Produto encontrado com sucesso!",
+                    LocalDateTime.now(),
+                    clientesResponse
+            );
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            Response<List<ProdutoResponse>> response = new Response<>(
+                    "Erro",
+                    e.getMessage(),
+                    LocalDateTime.now(),
+                    null
+            );
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Response<ProdutoResponse>> editar(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+        try {
+            ProdutoResponse  produtoEditadoResponse = produtoService.editar(id, produtoDTO);
+
+            Response<ProdutoResponse> response = new Response<>(
+                    "Sucesso",
+                    "Dados do produto editado com sucesso!",
+                    LocalDateTime.now(),
+                    produtoEditadoResponse
+            );
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            Response<ProdutoResponse> response = new Response<>(
+                    "Erro",
+                    e.getMessage(),
+                    LocalDateTime.now(),
+                    null
+            );
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response<Void>> deletar(@PathVariable Long id) {
+        try {
+            produtoService.deletar(id);
+
+            Response<Void> response = new Response<>(
+                    "Sucesso",
+                    "Produto deletado com sucesso!",
+                    LocalDateTime.now()
+            );
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+
+            Response<Void> response = new Response<>(
+                    "Erro",
+                    e.getMessage(),
+                    LocalDateTime.now()
+            );
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
 
 }
--- src/main/java/com/wdmcell/TecSystem/Service/ProdutoService.java
+++ src/main/java/com/wdmcell/TecSystem/Service/ProdutoService.java
@@ -1,12 +1,19 @@
 package com.wdmcell.TecSystem.Service;
 
+import com.wdmcell.TecSystem.DTO.ClienteDTO;
 import com.wdmcell.TecSystem.DTO.ProdutoDTO;
+import com.wdmcell.TecSystem.DTO.Response.ClienteResponse;
 import com.wdmcell.TecSystem.DTO.Response.ProdutoResponse;
+import com.wdmcell.TecSystem.Model.Cliente;
 import com.wdmcell.TecSystem.Model.Especificacao;
 import com.wdmcell.TecSystem.Model.Produto;
 import com.wdmcell.TecSystem.Repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -53,5 +60,108 @@ public class ProdutoService {
                 produtoCadastrado.getEspecificacao().getEspessura(),
                 produtoCadastrado.getEspecificacao().getPeso()
         );
+    }
+
+    public List<ProdutoResponse> buscarClientes() {
+        List<Produto> produtos = produtoRepository.findAll();
+        List<ProdutoResponse> responseProdutos = new ArrayList<>();
+
+        if (produtos.isEmpty()) {
+            throw new RuntimeException("Produtos não encontrados");
+        } else {
+            for (Produto produto : produtos) {
+                ProdutoResponse produtoResponse = new ProdutoResponse(
+                        produto.getId(),
+                        produto.getMarca(),
+                        produto.getModelo(),
+                        produto.getPreco_compra(),
+                        produto.getPreco_venda(),
+                        produto.getEstoque(),
+                        produto.getEspecificacao().getTela(),
+                        produto.getEspecificacao().getProcessador(),
+                        produto.getEspecificacao().getArmazenamento(),
+                        produto.getEspecificacao().getMemoria(),
+                        produto.getEspecificacao().getBateria(),
+                        produto.getEspecificacao().getSistema(),
+                        produto.getEspecificacao().getEspessura(),
+                        produto.getEspecificacao().getPeso()
+                );
+                responseProdutos.add(produtoResponse);
+            }
+            return responseProdutos;
+        }
+    }
+
+    public ProdutoResponse buscarPorId(Long id) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+
+        return new ProdutoResponse(
+                produto.getId(),
+                produto.getMarca(),
+                produto.getModelo(),
+                produto.getPreco_compra(),
+                produto.getPreco_venda(),
+                produto.getEstoque(),
+                produto.getEspecificacao().getTela(),
+                produto.getEspecificacao().getProcessador(),
+                produto.getEspecificacao().getArmazenamento(),
+                produto.getEspecificacao().getMemoria(),
+                produto.getEspecificacao().getBateria(),
+                produto.getEspecificacao().getSistema(),
+                produto.getEspecificacao().getEspessura(),
+                produto.getEspecificacao().getPeso()
+        );
+    }
+
+    public ProdutoResponse editar(Long id, ProdutoDTO produtoDTO) {
+        Optional<Produto> clienteEditar = produtoRepository.findById(id);
+
+        if (clienteEditar.isEmpty()) {
+            throw new RuntimeException("Erro ao tentar editar dados do produto");
+        } else {
+            Produto produtoEditar = clienteEditar.get();
+
+            produtoEditar.setMarca(produtoDTO.getMarca());
+            produtoEditar.setModelo(produtoDTO.getModelo());
+            produtoEditar.setPreco_compra(produtoDTO.getPreco_compra());
+            produtoEditar.setPreco_venda(produtoDTO.getPreco_venda());
+            produtoEditar.setEstoque(produtoDTO.getEstoque());
+
+            produtoEditar.getEspecificacao().setTela(produtoDTO.getTela());
+            produtoEditar.getEspecificacao().setBateria(produtoDTO.getBateria());
+            produtoEditar.getEspecificacao().setEspessura(produtoDTO.getEspessura());
+            produtoEditar.getEspecificacao().setPeso(produtoDTO.getPeso());
+            produtoEditar.getEspecificacao().setArmazenamento(produtoDTO.getArmazenamento());
+            produtoEditar.getEspecificacao().setMemoria(produtoDTO.getMemoria());
+            produtoEditar.getEspecificacao().setSistema(produtoDTO.getSistema());
+
+            Produto produtoEditado = produtoRepository.save(produtoEditar);
+
+            return new ProdutoResponse(
+                    produtoEditado.getId(),
+                    produtoEditado.getMarca(),
+                    produtoEditado.getModelo(),
+                    produtoEditado.getPreco_compra(),
+                    produtoEditado.getPreco_venda(),
+                    produtoEditado.getEstoque(),
+
+                    produtoEditado.getEspecificacao().getTela(),
+                    produtoEditado.getEspecificacao().getProcessador(),
+                    produtoEditado.getEspecificacao().getArmazenamento(),
+                    produtoEditado.getEspecificacao().getMemoria(),
+                    produtoEditado.getEspecificacao().getBateria(),
+                    produtoEditado.getEspecificacao().getSistema(),
+                    produtoEditado.getEspecificacao().getEspessura(),
+                    produtoEditado.getEspecificacao().getPeso()
+            );
+        }
+    }
+
+    public void deletar(Long id) {
+        if (!produtoRepository.existsById(id)) {
+            throw new RuntimeException("Produto não existe");
+        }
+        produtoRepository.deleteById(id);
     }
 }
