5bc74538968d9622ac6adc98f351a4cb04176eba
MSantos4 <mraiza4@gmail.com>
2025-12-11 14:18:08 -0300

    Adicionado serviço de login com cookie

--- src/main/java/com/wdmcell/TecSystem/Repository/FuncionarioRepository.java
+++ src/main/java/com/wdmcell/TecSystem/Repository/FuncionarioRepository.java
@@ -2,6 +2,9 @@ package com.wdmcell.TecSystem.Repository;
 
 import com.wdmcell.TecSystem.Model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FuncionarioRepository  extends JpaRepository<Funcionario, Long> {
+    @Query("SELECT f FROM Funcionario f JOIN FETCH f.login l where l.usuario  = :usuario AND l.senha = :senha ")
+    Funcionario  findByUsuarioESenha(String usuario, String  senha);
 }
---
+++ src/main/java/com/wdmcell/TecSystem/Service/LoginService.java
@@ -0,0 +1,28 @@
+package com.wdmcell.TecSystem.Service;
+
+import com.wdmcell.TecSystem.DTO.LoginDTO;
+import com.wdmcell.TecSystem.DTO.LoginResponseDTO;
+import com.wdmcell.TecSystem.Model.Funcionario;
+import com.wdmcell.TecSystem.Repository.FuncionarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+   private final  FuncionarioRepository funcionarioRepository;
+
+   public LoginResponseDTO login(LoginDTO loginDTO) {
+       Funcionario funcionario = funcionarioRepository.findByUsuarioESenha(loginDTO.getUsuario(), loginDTO.getSenha());
+
+       if (funcionario != null) {
+           return new LoginResponseDTO(
+                   funcionario.getNome(),
+                   funcionario.getLogin().getNivel_permissao(),
+                   funcionario.getMatricula()
+                   );
+       }
+    return null;
+   }
+
+}
