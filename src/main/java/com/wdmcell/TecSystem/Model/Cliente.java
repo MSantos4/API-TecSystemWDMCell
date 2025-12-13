8c50429a6f270fe1eee28521b3383fba75d64a53
MSantos4 <mraiza4@gmail.com>
2025-12-13 15:00:36 -0300

    alterações nas colunas

--- src/main/java/com/wdmcell/TecSystem/Model/Cliente.java
+++ src/main/java/com/wdmcell/TecSystem/Model/Cliente.java
@@ -12,8 +12,12 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 120, nullable = false)
     private String nome;
+    @Column(length = 14, unique = true, nullable = false)
     private String cpf;
+    @Column(length = 15, nullable = false)
     private String telefone;
     private String email;
 
